9d810ede77a68dcda3589ea2b22cc63ac8ce9009
Amaksik <a.maksik2013@gmail.com>
2022-01-31 13:33:07 +0200

    adding correct response codes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var express = require('express');
 var cors = require('cors')
 var userscontroller = require("./controller_users")
@@ -7,13 +8,36 @@ var countriescontroller = require("./controller_countries")
 var app = express();
 let port = process.env.PORT || 3000;
 
+
+
 app.use(express.json())    // <==== parse request body as JSON
+
 app.use(cors())
 
 app.get('/countries', function(req, res) {
   res = addheaders(res);
   res.send(countriescontroller.getcountries());
 });
+app.post('/countries', cors(), function(req, res) {
+  let newcountry = req.body;
+  
+  if(!newcountry){
+    res.status(400)
+    res.send("Country is not provided");}
+  else{
+    try{
+      countriescontroller.addcountry(newcountry)
+      res.status(201)
+      res.send(JSON.stringify(`${newcountry} added`));
+    }
+    catch{
+        res.status(409)
+        res.send("country already exists");
+    }
+    
+  }
+});
+
 
 app.get('/users', function(req, res) {
   res = addheaders(res);
@@ -24,8 +48,8 @@ app.post('/users/autorize', cors(), function(req, res) {
   let credentials = req.body;
   
   if(!credentials){
-    res.status(404)
-    res.send("not provided");}
+    res.status(400)
+    res.send("Credentials are not provided");}
   else{
     try{
       let id =  userscontroller.autorize(credentials.email, credentials.password);
@@ -34,7 +58,7 @@ app.post('/users/autorize', cors(), function(req, res) {
     }
     catch{
         res.status(404)
-        res.send("user not found");
+        res.send("User not found");
     }
     
   }
@@ -43,7 +67,13 @@ app.post('/users/autorize', cors(), function(req, res) {
 
 app.get('/users/:id', function(req, res) {
   res = addheaders(res);
-  res.send(userscontroller.getUserById(req.params.id));
+  try{
+    res.status(200).send(userscontroller.getUserById(req.params.id));
+  }
+  catch{
+    res.status(404).send("User not found");
+  }
+  
 
 });
 app.put('/users/:id', function(req, res) {
@@ -56,29 +86,31 @@ app.put('/users/:id', function(req, res) {
 
 app.delete('/users/:id', function(req, res) {
   res = addheaders(res);
-  userscontroller.removeUser(req.params.id);
-  res.sendStatus(200);
+  try{
+    userscontroller.removeUser(req.params.id);
+    res.sendStatus(200);
+  }
+  catch{
+    res.status(404).send("User not found");
+  }
+
 });
 
 
 app.post('/users', cors(), function(req, res) {
   let newuser = req.body;
-  console.log("1");
   res = addheaders(res);
   if(!newuser){
-    console.log("2");
-    res.status(404)
+    res.status(400)
     res.send("not provided");}
   else{
     try{
-      console.log("3");
       userscontroller.getUserId(newuser.name, newuser.surname);
-      res.status(400)
+      res.status(409)
       res.send("already exists");
     }
     catch{
       try{
-        console.log("4");
         newuser.name;
         newuser.surname;
         newuser.age;
@@ -88,8 +120,7 @@ app.post('/users', cors(), function(req, res) {
       res.send(JSON.stringify(userid));
       }
       catch{
-        console.log("5");
-        res.status(400)
+        res.status(422)
         res.send("not enough info");
       }
       
@@ -105,7 +136,7 @@ app.get('/', function(req, res) {
 
 
 app.listen(port, function() {
-  console.log(`Example app listening on port http://localhost:${port}!`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
 
 function addheaders(obj){
